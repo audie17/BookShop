c11b66c51f401344e707e29af132929426796ed9
charlineblanc <charline.blanc@efrei.net>
2023-12-04 00:37:15 +0200

    remove unnecessary files + correct main

--- src/main/java/fr/efrei/Main.java
+++ src/main/java/fr/efrei/Main.java
@@ -10,11 +10,9 @@ public class Main {
         int choice;
         do {
             System.out.println("\u001B[34m"+"「 ✦ Main Menu:\n"+"\u001B[0m");
-            System.out.println("1. Book Shop\n");
-            System.out.println("2. Book\n");
-            System.out.println("3. Shopping Cart\n");
-            System.out.println("4. Employee\n");
-            System.out.println("5. Customers\n");
+            System.out.println("1. Book\n");
+            System.out.println("2. Shopping Cart\n");
+            System.out.println("3. Customers\n");
             System.out.println("0. Exit\n");
 
             System.out.print("Enter your choice: ");
@@ -23,26 +21,17 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    BookShopMenu bookShopMenu = new BookShopMenu();
-                    bookShopMenu.runBookShop();
-                    break;
-
-                case 2:
                     BookMenu bookMenu = new BookMenu();
                     bookMenu.runBook();
                     break;
 
-                case 3:
+                case 2:
                     ShoppingCartMenu shoppingCartMenu = new ShoppingCartMenu();
                     shoppingCartMenu.runShoppingCart();
                     break;
 
-                case 4:
-                    EmployeeMenu employeeMenu = new EmployeeMenu();
-                    employeeMenu.runEmployee();
-                    break;
 
-                case 5:
+                case 3:
                     CustomerMenu customerMenu = new CustomerMenu();
                     customerMenu.runCustomer();
                     break;
